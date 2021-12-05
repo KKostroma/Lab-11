a98041662ef66fc22d813f5f849d9961af5f76c0
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2021-12-05 23:20:12 +0300

    Добавьте файлы проекта.

---
+++ Lab 11/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11
+{
+    public class Yravnenie
+    {
+        public float k;
+        public float b;
+        public double x;
+
+
+        public Yravnenie(float k, float b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public void Root()
+        {
+            double x = -b / k;
+            Console.WriteLine("Ответ {0}", x);
+        }
+    }
+}
